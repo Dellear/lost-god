f9c464ab81f8b71bc78368aafe497ca4cd6dbee6
wangsq <358009775@qq.com>
2021-06-14 19:09:05 +0800

    reset add fetch

--- jup.sh
+++ jup.sh
@@ -54,6 +54,7 @@ reset_romote_url () {
     if [ -d "$dir_work/.git" ]; then
         cd $dir_work
         git remote set-url origin $url >/dev/null
+        git fetch --all
         git reset --hard origin/$branch >/dev/null
         cd $dir_current
     fi
